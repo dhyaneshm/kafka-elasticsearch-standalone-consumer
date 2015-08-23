b7788384bdfe440df9dd41f7b1cefded68ecd63e
Dhyan <d.muralidharan@yottaa.com>
2015-08-23 13:09:12 -0400

    commit to create a sample unit test

---
+++ src/test/java/org/elasticsearch/kafka/indexer/messageHandlers/RawMessageStringHandlerTest.java
@@ -0,0 +1,16 @@
+package org.elasticsearch.kafka.indexer.messageHandlers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RawMessageStringHandlerTest  {
+
+
+    private RawMessageStringHandler rawMessageStringHandler ;
+
+    @Test
+    public void testTransformMessage(){
+        Assert.assertEquals(1,1);
+    }
+
+}
